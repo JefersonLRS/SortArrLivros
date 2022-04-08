7b7213682091a73e1fa3fe92824bee7a89027724
Jeferson <contato.jeferson@gmail.com>
2022-04-08 08:34:22 -0300

    insertionSort Arrays

---
+++ listaLivros.js
@@ -0,0 +1,21 @@
+const precosLivros = [
+    
+    {titulo: "JavaScpit",
+    preco: 25},
+
+    {titulo: "PHP",
+    preco: 15},
+
+    {titulo: "Java",
+    preco: 30},
+
+    {titulo: "Go",
+    preco: 45},
+    
+    {titulo: "Elixir",
+    preco: 50}, 
+    
+    {titulo: "Phyton",
+    preco: 20}];
+
+    module.exports = precosLivros;
